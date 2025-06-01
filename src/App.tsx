b18a6813b7e35cda67395cb7fdd18f2efa11c44e
AbdelrahmanW08 <abdelrahman.wm2@gmail.com>
2025-06-01 17:53:20 -0400

    initialized website

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className="container">
+        <div className="greeting">
+          <h1>hey, this is Abdelrahman. 👋</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
