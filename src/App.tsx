883201e13ad7edf6f33d9db707aea8503108d37d
AbdelrahmanW08 <abdelrahman.wm2@gmail.com>
2025-06-01 19:52:52 -0400

    made basic layout of website

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
+import { FileUser, MapPin, Linkedin, Github, Mail } from 'lucide-react';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
       <div className="container">
-        <div className="greeting">
-          <h1>hey, this is Abdelrahman. 👋</h1>
+        <div className="content-section">
+          <div className="greeting">
+            <h1>hey, this is Abdelrahman. 👋</h1>
+            <p className="subtitle"><MapPin size={20} /> Ontario, Canada</p>
+            <p className="subtitle">19yo software developer and ml engineer.</p>
+            <p className="subtitle second-subtitle">Exploring the tech world since 2010. </p>
+            <div className="buttons-container">
+              <button className="resume-button">
+                Resume <FileUser size={20} />
+              </button>
+              <button className="icon-button">
+                <a href="https://www.linkedin.com/in/abdelrahman-wm/" target="_blank">
+                  <Linkedin size={20} />
+                </a>
+              </button>
+              <button className="icon-button">
+                <a href="https://github.com/AbdelrahmanW08" target="_blank">
+                  <Github size={20} />
+                </a>
+              </button>
+              <button className="icon-button">
+                <a href="amoustafa@torontomu.ca" target="_blank">
+                  <Mail size={20} />
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="image-placeholder">
+          {/* Placeholder for image */}
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Linkedin, Github, Mail } from 'lucide-react';
+
+function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-text">
+        © {new Date().getFullYear()} <a className='footer-link' href="https://amoustafa.me">amoustafa.me</a>
+      </div>
+      <div className="buttons-container">
+        <button className="icon-button">
+          <a href="https://www.linkedin.com/in/abdelrahman-wm/" target="_blank">
+            <Linkedin size={20} />
+          </a>
+        </button>
+        <button className="icon-button">
+          <a href="https://github.com/AbdelrahmanW08" target="_blank">
+            <Github size={20} />
+          </a>
+        </button>
+        <button className="icon-button">
+          <a href="amoustafa@torontomu.ca" target="_blank">
+            <Mail size={20} />
+          </a>
+        </button>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
